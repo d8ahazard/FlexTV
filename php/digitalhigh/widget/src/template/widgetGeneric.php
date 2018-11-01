57bed6d73ba6f67daf5994f5a135bd0e441be13d
d8ahazard <d8ahazard@gmail.com>
2018-11-01 18:47:53 -0500

    Add Sample Header to generic widget

--- php/digitalhigh/widget/src/template/widgetGeneric.php
+++ php/digitalhigh/widget/src/template/widgetGeneric.php
@@ -28,6 +28,22 @@ class widgetGeneric {
 				<div class="card m-0 card-rotate card-background">
 					<div class="front front-background">
 						<!-- Card front markup goes in here -->
+						<h4 class="card-header d-flex justify-content-between align-items-center text-white px-3">
+							<span class="d-flex align-items-center">
+							<i class="material-icons editItem">drag_indicator</i></span>Generic 
+							<span>
+								<button type="button" class="btn btn-settings editItem" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<i class="material-icons">more_vert</i>
+								</button>
+								<div class="dropdown-menu dropdown-menu-right">
+									<button class="dropdown-item" type="button">Edit</button>
+									<button class="dropdown-item" type="button">Refresh</button>
+									<div class="dropdown-divider"></div>
+									<button class="dropdown-item" type="button">Delete</button>
+								</div>
+							</span>
+						</h4>
+						
 					</div>
 					<div class="back card-rotate back-background">
 		                <div class="widgetHandle btn">
